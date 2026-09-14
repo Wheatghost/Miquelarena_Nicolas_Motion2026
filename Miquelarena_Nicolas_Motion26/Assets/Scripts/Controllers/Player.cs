460d41e7a376dda0b2b694d00a9193589c547ecd
miquelan <miquelan@OA-S142-07>
2026-09-14 17:17:40 -0400

    Week 2 stuff

---
+++ Miquelarena_Nicolas_Motion26/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> asteroidTransforms;
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+    public Vector2 spawnOffset; //offset variable
+    public Transform playerPos; //player position and everything
+
+    void Update()
+    {
+        
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            SpawnBombAtOffset(spawnOffset);
+            Debug.Log(spawnOffset);
+        }
+
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            Warp(enemyTransform, playerPos);
+        }
+
+    }
+
+    void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        //instantiate bombprefab at vector3 inOffset
+    }
+
+    void Warp(Transform target, Transform pos)
+    {
+        //Take the target's position, find the distance and direction, translate the player towards those coordinates 
+    }
+}
