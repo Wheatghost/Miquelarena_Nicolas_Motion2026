d1473ccd6bc30f9dae2a1671f8f5313d52f7ec5a
Wheatghost <nmiquelarena@yahoo.com>
2026-09-20 14:59:40 -0400

    Got the bomb spawner mostly working, couldnt get the line of bombs to work

--- Miquelarena_Nicolas_Motion26/Assets/Scripts/Controllers/Player.cs
+++ Miquelarena_Nicolas_Motion26/Assets/Scripts/Controllers/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,16 +10,24 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
-    public Vector2 spawnOffset; //offset variable
+
     public Transform playerPos; //player position and everything
+
+    public Vector2 spawnOffset; //offset variable
+    public Vector2 pos;
+
+    public float numberOfBombs; //number of bombs
 
     void Update()
     {
-        
+        pos = playerPos.position;//for some reason this needs to be done as a seperate step or else it gets mad at me
+
         if (Keyboard.current.bKey.wasPressedThisFrame)
         {
-            SpawnBombAtOffset(spawnOffset);
-            Debug.Log(spawnOffset);
+            //Update the bomb offset postion by using Player position + Spawnoffset
+            bombsTransform.position = (Vector2)pos + spawnOffset;
+            SpawnBombAtOffset(bombsTransform, numberOfBombs);
+            Debug.Log(playerPos.position);
         }
 
         if (Keyboard.current.wKey.wasPressedThisFrame)
@@ -28,9 +37,14 @@ public class Player : MonoBehaviour
 
     }
 
-    void SpawnBombAtOffset(Vector3 inOffset)
+    void SpawnBombAtOffset(Transform bombOffset, float amount)
     {
+        Vector2 offset = new Vector2 (0, -0.2f);
         //instantiate bombprefab at vector3 inOffset
+        for (int i = 0; i<amount; i++) {
+            Instantiate(bombPrefab, bombOffset);
+            bombOffset.position = (Vector2)bombOffset.position + offset;
+        }
     }
 
     void Warp(Transform target, Transform pos)
