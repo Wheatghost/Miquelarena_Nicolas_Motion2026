eaef2a1cf719f81179db15d1620940f8b4d187b9
Wheatghost <nmiquelarena@yahoo.com>
2026-09-20 15:18:12 -0400

    Added CornerBomb Spawning

--- Miquelarena_Nicolas_Motion26/Assets/Scripts/Controllers/Player.cs
+++ Miquelarena_Nicolas_Motion26/Assets/Scripts/Controllers/Player.cs
@@ -11,12 +11,16 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    //Task 1
     public Transform playerPos; //player position and everything
 
     public Vector2 spawnOffset; //offset variable
     public Vector2 pos;
 
     public float numberOfBombs; //number of bombs
+
+    //Task 2
+    public Vector2 distance; //distance from the player to the corner
 
     void Update()
     {
@@ -28,6 +32,13 @@ public class Player : MonoBehaviour
             bombsTransform.position = (Vector2)pos + spawnOffset;
             SpawnBombAtOffset(bombsTransform, numberOfBombs);
             Debug.Log(playerPos.position);
+        }
+
+        if (Keyboard.current.vKey.wasPressedThisFrame)
+        {
+            bombsTransform.position = playerPos.position;
+            //spawn a corner bomb
+            SpawnCornerBomb(bombsTransform, distance);
         }
 
         if (Keyboard.current.wKey.wasPressedThisFrame)
@@ -47,8 +58,38 @@ public class Player : MonoBehaviour
         }
     }
 
-    void Warp(Transform target, Transform pos)
+    void SpawnCornerBomb(Transform position, Vector2 distance)
     {
-        //Take the target's position, find the distance and direction, translate the player towards those coordinates 
+        int corner = Random.Range(0, 4); //randomly select a corner, each time the method is called
+        // randomly pick a corner, and spawn a bomb at a fixed distance from the player
+        if (corner == 0)//top left (-x)
+        {
+            distance.x *= -1;
+            position.position = (Vector2)position.position + distance;
+            Instantiate(bombPrefab, position);
+        }
+        else if (corner == 1)//top right (no change)
+        {
+            position.position = (Vector2)position.position + distance;
+            Instantiate(bombPrefab, position);
+        }
+        else if (corner == 2)//bottom right (-y)
+        {
+            distance.y *= -1;
+            position.position = (Vector2)position.position + distance;
+            Instantiate(bombPrefab, position);
+        }
+        else if (corner == 3)//bottom left (-x,-y)
+        {
+            distance *= -1;
+            position.position = (Vector2)position.position + distance;
+            Instantiate(bombPrefab, position);
+        }
     }
+
+
+            void Warp(Transform target, Transform pos)
+            {
+                //Take the target's position, find the distance and direction, translate the player towards those coordinates 
+            }
 }
